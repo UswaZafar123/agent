27c6f801bc1901a7d4e5f5cf1aacf8d98e3a33a1
Florent BALKOULGA <bfo226@gmail.com>
2021-08-17 14:39:06 +0100

    Langues

--- src/Component/Agent/login.js
+++ src/Component/Agent/login.js
@@ -14,7 +14,7 @@ class Login extends Component {
     this.state = {
       latitude: "",
       longitude: "",
-      browser: "",
+      browser: "", 
       os: "",
       ipAddress: "",
       email: "",
--- src/Component/Banking/transfer.js
+++ src/Component/Banking/transfer.js
@@ -32,7 +32,7 @@ import Checkbox from "@material-ui/core/Checkbox";
 import success from '../../Assets/images/confirm.svg';
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
-import {MenuItem} from "@material-ui/core";
+import MenuItem from "@material-ui/core/MenuItem";
 import Typography from "@material-ui/core/Typography";
 import {Card} from "reactstrap";
 
