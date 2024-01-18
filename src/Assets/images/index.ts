0935124546b3f355ad5574767cb7adefcdcb8380
ericson949 <ericson949@gmail.com>
2024-01-18 18:59:14 +0100

    before commit

---
+++ src/Assets/images/index.ts
@@ -0,0 +1,13 @@
+const IMAGES = {
+    ClientImage: new URL("./clientImage.png", import.meta.url).href,
+    AdminImage: new URL("./admin-settings-male.png", import.meta.url).href,
+    AgentImage: new URL("./Group.png", import.meta.url).href,
+    MerchantImage: new URL("./MerchantRegister.png", import.meta.url).href,
+    LOGO_SMALL: new URL("./logoSmall.png", import.meta.url).href,
+    LOGO: new URL("./logoBlack.png", import.meta.url).href,
+    LOGOWhite: new URL("./logo.jpg", import.meta.url).href,
+    Image1: new URL("./manager 1.png", import.meta.url).href,
+    Image3: new URL("./Group (1).png", import.meta.url).href,
+};
+
+export default IMAGES;
